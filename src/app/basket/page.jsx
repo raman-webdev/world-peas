f92d2c6f9427155913affb293a32a3d411d10311
raman-webdev <chaudharyraman562@gmail.com>
2024-12-31 13:10:56 +0545

    update

--- src/app/basket/page.jsx
+++ src/app/basket/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../header/Header'
 import Image from 'next/image'
+import { LuPencil } from "react-icons/lu";
+import { BsArrowRight } from "react-icons/bs";
 
 const Basket = () => {
   return (
@@ -16,25 +18,114 @@ const Basket = () => {
         <div className='mt-6 h-[2px] bg-gray-300'></div>
          
          {/* -------- */}
-        <div className='mt-12'>
-          <div className=' w-[800px] bg-yellow-50 border  rounded-2xl inline-flex'>
-           <Image
-            src="/Assets/tomato.png"
-            width={500}
-            height={500}
-            style={{objectFit:"contain"}}
-            className='h-24 w-auto rounded-tl-2xl rounded-bl-2xl'
-            alt="Picture of the author"
-           />
-           <div className='p-3'>
-            <div className=' font-semibold flex justify-between' >
-             <p >Heirloom tomato</p>
-             <h2>$5.99</h2>
+
+         <div className="flex">
+          <div className='flex-row'>
+            <div className='mt-12'>
+              <div className=' w-[800px] bg-yellow-50 border  rounded-2xl inline-flex'>
+              <Image
+                src="/Assets/tomato.png"
+                width={500}
+                height={500}
+                style={{objectFit:"cover"}}
+                className='h-24 w-24 rounded-tl-2xl rounded-bl-2xl'
+                alt="Picture of the author"
+              />
+              <div className='p-3'>
+                <div className=' font-semibold flex justify-between gap-x-[480px]' >
+                <p >Heirloom tomato</p>
+                <h2>$5.99</h2>
+                </div>
+                <h2>$5.99 /lb</h2>
+                <div className='flex items-center justify-around gap-4 bg-white  w-24 rounded-2xl'>
+                  <p>1 lb</p>
+                  <LuPencil />
+                </div>
+              </div> 
+              </div>
             </div>
-            <h2>$5.99 /lb</h2>
-           </div> 
+
+            <div className='mt-6'>
+              <div className=' w-[800px] bg-yellow-50 border  rounded-2xl inline-flex'>
+              <Image
+                src="/Assets/ginger.png"
+                width={500}
+                height={500}
+                style={{objectFit:"contain"}}
+                className='h-24 w-auto rounded-tl-2xl rounded-bl-2xl'
+                alt="Picture of the author"
+              />
+              <div className='p-3'>
+                <div className=' font-semibold flex justify-between gap-x-[480px]' >
+                <p >Heirloom tomato</p>
+                <h2>$5.99</h2>
+                </div>
+                <h2>$5.99 /lb</h2>
+                <div className='flex items-center justify-around gap-4 bg-white  w-24 rounded-2xl'>
+                  <p>1 lb</p>
+                  <LuPencil />
+                </div>
+              </div> 
+              </div>
+            </div>
+
+            <div className='mt-6'>
+              <div className=' w-[800px] bg-yellow-50 border  rounded-2xl inline-flex'>
+              <Image
+                src="/Assets/onion.png"
+                width={500}
+                height={500}
+                style={{objectFit:"contain"}}
+                className='h-24 w-auto rounded-tl-2xl rounded-bl-2xl'
+                alt="Picture of the author"
+              />
+              <div className='p-3'>
+                <div className=' font-semibold flex justify-between gap-x-[480px]' >
+                <p >Heirloom tomato</p>
+                <h2>$5.99</h2>
+                </div>
+                <h2>$5.99 /lb</h2>
+                <div className='flex items-center justify-around gap-4 bg-white  w-24 rounded-2xl'>
+                  <p>1 lb</p>
+                  <LuPencil />
+                </div>
+              </div> 
+              </div>
+            </div>
+
+        
           </div>
-        </div>
+          <div className="bg-yellow-50 w-72 h-60 p-3 mt-12 ml-36 border rounded-2xl ">
+            <div>
+              <h2 className='font-semibold'>Order Summary</h2>
+            </div>
+            <div className='mt-2 flex gap-36'>
+              <p>Subtotal</p>
+              <p>$27.44</p>
+            </div>
+            <div className='mt-2 flex gap-36'>
+              <p>Subtotal</p>
+              <p>$27.44</p>
+            </div>
+            <div className='mt-2 flex gap-36'>
+              <p>Subtotal</p>
+              <p>$27.44</p>
+            </div>
+            <div className='mt-2 flex gap-36'>
+              <p>Subtotal</p>
+              <p>$27.44</p>
+            </div>
+
+            <div className='bg-green-600 w-64 mt-4 rounded-md'>
+              <div className='flex items-center justify-between px-3 py-2 text-white '>
+                <h3>Continue to Payment</h3>
+                <BsArrowRight />
+              </div>
+            </div>
+          </div>
+        </div>  
+    
+        
       </div>
 
     </>
